171536fe65d40eb34ab63aa879e4095d6f163618
abdo <abdomedono2014@gmail.com>
2019-04-28 15:46:25 +0200

    added aluReservationStations

--- DataRam.vhd
+++ DataRam.vhd
@@ -4,17 +4,17 @@ use IEEE.numeric_std.all;
 
 entity DataRam is
 	port(
-		clk :			in std_logic;
+		clk:				in std_logic;
 		readWriteEnable:	in std_logic;
-		address :		in  std_logic_vector(6 downto 0);
-		dataIn :		in std_logic_vector(15 downto 0);
-		dataOut :		OUT std_logic_vector(15 downto 0));
+		address:			in  std_logic_vector(6 downto 0);
+		dataIn:				in std_logic_vector(15 downto 0);
+		dataOut:			out std_logic_vector(15 downto 0)
+	);
 end entity DataRam;
 
 architecture rtl of DataRam is
-
-	TYPE ram_type is array(0 to 65535) of std_logic_vector(15 downto 0);
-	SIGNAL ram : ram_type := (others => (others => '0'));
+	type ram_type is array(0 to 65535) of std_logic_vector(15 downto 0);
+	signal ram : ram_type := (others => (others => '0'));
 	
 	begin
 		process(clk) is
@@ -22,7 +22,6 @@ architecture rtl of DataRam is
 				if clk'event and clk = '1' then  
 					if readWriteEnable = '1' then
 						ram(to_integer(unsigned(address))) <= dataIn;
-					
 					else
 						dataOut <= ram(to_integer(unsigned(address)));
 					end if;
--- ReservationStation.vhd
+++ ReservationStation.vhd
@@ -2,23 +2,107 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
-entity ReservationStation is 
+entity ReservationStation is
+	generic (n: integer := 16);
 	port(
-		tagName  :    		inout std_logic_vector(3 downto 0);
-		opCode   :		in std_logic_vector(4 downto 0);
-		src1Tag  :		inout std_logic_vector(15 downto 0);
-		src1Valid:		inout std_logic := '0';
-		src2Tag  :		inout std_logic_vector(15 downto 0);
-		src2Valid:		inout std_logic := '0';
-		ready    :		out std_logic := '0';
-		busy:			inout std_logic := '0'
+		ROBName:    out std_logic_vector(3 downto 0);
+		destName:   in std_logic_vector(3 downto 0);
+		opCode:		in std_logic_vector(4 downto 0);
+		src1Tag:	inout std_logic_vector(15 downto 0);
+		src1Valid:	inout std_logic := '0';
+		src2Tag:	inout std_logic_vector(15 downto 0);
+		src2Valid:	inout std_logic := '0';
+		enables:	in std_logic_vector(4 downto 0);
+		ready:		out std_logic := '1;
+		busy:		inout std_logic := '0';
+		clk:		in std_logic;
+		reset:		in std_logic
 	);
 end entity ReservationStation;
 
-architecture rtl of ReservationStation is 
-	begin 
+architecture rtl of ReservationStation is
+	signal srcRegTagInput1:		std_logic_vector(n-1 downto 0) := (others => '0');
+	signal srcRegTagEnable1:	std_logic := '0';
+	signal srcRegTagReset1:		std_logic := '0';
+	signal srcRegTagOutput1:	std_logic_vector(n-1 downto 0) := (others => '0');
 
+	signal srcRegTagInput2:		std_logic_vector(n-1 downto 0) := (others => '0');
+	signal srcRegTagEnable2:	std_logic := '0';
+	signal srcRegTagReset2:		std_logic := '0';
+	signal srcRegTagOutput2:	std_logic_vector(n-1 downto 0) := (others => '0');
+
+	signal srcRegValidInput1:	std_logic_vector(0 downto 0) := (others => '0');
+	signal srcRegValidEnable1:	std_logic := '0';
+	signal srcRegValidReset1:	std_logic := '0';
+	signal srcRegValidOutput1:	std_logic_vector(0 downto 0) := (others => '0');
+
+	signal srcRegValidInput2:	std_logic_vector(0 downto 0) := (others => '0');
+	signal srcRegValidEnable2:	std_logic := '0';
+	signal srcRegValidReset2:	std_logic := '0';
+	signal srcRegValidOutput2:	std_logic_vector(0 downto 0) := (others => '0');
+
+	signal busyRegInput:	std_logic_vector(0 downto 0) := (others => '0');
+	signal busyRegEnable:	std_logic := '0';
+	signal busyRegReset:	std_logic := '0';
+	signal busyRegOutput:	std_logic_vector(0 downto 0) := (others => '0');
+
+	begin
+		srcRegTag1: entity work.mRegister
+			generic map (n => n)
+			port map (
+				input	=> srcRegTagInput1,
+				enable	=> srcRegTagEnable1,
+				clk		=> clk,
+				reset	=> srcRegTagReset1,
+				output	=> srcRegTagOutput1
+			);
+
+		srcRegTag2: entity work.mRegister
+			generic map (n => n)
+			port map (
+				input	=> srcRegTagInput2,
+				enable	=> srcRegTagEnable2,
+				clk		=> clk,
+				reset	=> srcRegTagReset2,
+				output	=> srcRegTagOutput2
+			);
+
+		srcRegValid1: entity work.mRegister
+			generic map (n => 1)
+			port map (
+				input	=> srcRegValidInput1,
+				enable	=> srcRegValidEnable1,
+				clk		=> clk,
+				reset	=> srcRegValidReset1,
+				output	=> srcRegValidOutput1
+			);
+
+		srcRegValid2: entity work.mRegister
+			generic map (n => 1)
+			port map (
+				input	=> srcRegValidInput2,
+				enable	=> srcRegValidEnable2,
+				clk		=> clk,
+				reset	=> srcRegValidReset2,
+				output	=> srcRegValidOutput2
+			);
+		
+		busyReg: entity work.mRegister
+			generic map (n => 1)
+			port map (
+				input	=> busyRegInput,
+				enable	=> busyRegEnable,
+				clk		=> clk,
+				reset	=> busyRegReset,
+				output	=> busyRegOutput
+			);
+		
 		ready <= src1Valid and src2Valid;
+
+		process(clk, reset)
+		begin
+
+		end process;
 
 end architecture rtl;
 

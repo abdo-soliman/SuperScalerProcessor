7752cb542ac68c1ebf308e4daa8f6312392f5f9f
Ahmed Ibrahim <ahmed.abdelnaby98@eng-st.cu.edu.eg>
2019-04-28 16:08:41 +0200

    Fix bugs

--- InstructionQueue.vhd
+++ InstructionQueue.vhd
@@ -6,16 +6,16 @@ use IEEE.numeric_std.all;
 --length n and width m
 
 entity InstructionQueue is
-    generic ( length: integer := 8;
-    		 width: integer  := 32;
-    		 widthByTwo: integer := 16);
+    generic ( 	length: integer := 8;
+    		 	width: integer  := 32;
+    		 	widthByTwo: integer := 16);
     port (
-        input:  in      	std_logic_vector(width-1 downto 0) := (others => '0');
-        enable: in      	std_logic := '1'; --if ROB full don't enable
-        reset:  in      	std_logic := '0';
-        clk:    in      	std_logic := '0';
-        queueFull:	out 	std_logic := '0';
-        output: out     	std_logic_vector(widthByTwo-1 downto 0) := (others => '0')
+        input:  		in      	std_logic_vector(width-1 downto 0) := (others => '0');
+        enable: 		in      	std_logic := '1'; --if ROB full don't enable
+        reset:  		in      	std_logic := '0';
+        clk:    		in      	std_logic := '0';
+        queueFull:		out 		std_logic := '0';
+        output: 		out     	std_logic_vector(widthByTwo-1 downto 0) := (others => '0')
     );
 end entity InstructionQueue;
 
@@ -29,9 +29,11 @@ architecture rtl of InstructionQueue is
 	
 begin
     output <= q(0);
-    queueFullSignal <= '1' when (tail = "1000" or tail = "0111")
-    				else '0';
-    queueFull <= queueFullSignal;
+    --queueFullSignal <= '1' when (tail = "1000" or tail = "0111")
+    				--else '0';
+    queueFull <= '0' when ((queueFullSignal = '0') or ((enable = '1') and (tail = "0111")))
+    		else '1';
+
     process(clk,reset)
     begin
         if (reset = '1') then
@@ -39,6 +41,7 @@ begin
         	tail <= (others=> '0');
         elsif (clk'event and clk = '1') then --shift and output to ROB, ROB reads in negative edge
         	if(enable = '1') then
+        		q(length-1) <= (others => '0');
 	        	--Shifting
 	        	for i in length-2 downto 0 loop
 	        		q(i) <= q(i + 1);
@@ -46,6 +49,9 @@ begin
 
 	        	if (tail /= "0000") then
 	        		tail <= tail - 1;
+	        		if (tail = "0111") then
+	        			queueFullSignal <= '0';
+	        		end if;
 	        	end if;
        		end if;	
     	elsif (clk'event and clk = '0') then 
@@ -53,6 +59,11 @@ begin
 	        	q(to_integer(unsigned(tail))) <= input(31 downto 16);
 	        	q(to_integer(unsigned(tail))+1) <= input(15 downto 0);
 	        	tail <= tail + 2;
+	        	if (tail = "0110" or tail = "0101") then
+	        		queueFullSignal <= '1';
+	        	else
+	        		queueFullSignal <= '0';
+	        	end if;
     		end if;	
         end if;
     end process;
--- ReorderBuffer.vhd
+++ ReorderBuffer.vhd
@@ -5,52 +5,75 @@ use IEEE.numeric_std.all;
 
 --length n and width m
 
-entity InstructionQueue is
+entity ReorderBuffer is
     generic ( length: integer := 16;
-    		 width: integer  := 45
+    		 width: integer  := 16  --I changed this from 45 @Ahmed
     		 );
     port (
-        instruction:  in      	std_logic_vector(width-1 downto 0) := (others => '0');
-        aluTAG:	in      std_logic_vector(3 downto 0);
+        instruction:	in      	std_logic_vector(width-1 downto 0) := (others => '0');
+        --aluTAG:	in      std_logic_vector(3 downto 0);  --I commented this @Ahmed
         --Some other inputs
 
        	---------------------------------------------------------------------
-        reset:  in      	std_logic := '0';
-        clk:    in      	std_logic := '0';
-        ROBFull:	out 	std_logic := '0';
-        output: out     	std_logic_vector(widthByTwo-1 downto 0) := (others => '0')
+        reset:			in      	std_logic := '0';
+        clk:			in      	std_logic := '0';
+        ROBFull:		out 		std_logic := '0';
+        ROBEmpty:		out 		std_logic := '0';
+        output: 		out     	std_logic_vector(width-1 downto 0) := (others => '0')
     );
-end entity InstructionQueue;
+end entity ReorderBuffer;
 
-architecture rtl of InstructionQueue is
+architecture rtl of ReorderBuffer is
 	type qType is array(0 to length-1) of std_logic_vector(width-1 downto 0);
-    signal q 	: qType := (others => (others => '0'));
-    signal head	: std_logic_vector(3 downto 0) := (others => '0');
-    signal tail	: std_logic_vector(3 downto 0) := (others => '0');
-    signal ROBFullSignal: std_logic :='0';
+    signal q:	 					qType := (others => (others => '0'));
+    signal readPointer: 			std_logic_vector(3 downto 0) := (others => '0');
+    signal readPointerRotated:		std_logic := '0';
+    signal writePointer: 			std_logic_vector(3 downto 0) := (others => '0');
+    signal writePointerRotated:		std_logic := '0';
+    signal ROBFullSignal: 			std_logic := '0';
+    signal ROBEmptySignal: 			std_logic := '1';
     
 	
 	
 begin
-    output <= q(0);
-    ROBFullSignal <= '1' when tail = head
-    				else '0';
+    output <= q(to_integer(unsigned(readPointer)));
+    --ROBFullSignal <= '1' when tail = head
+    --				else '0';
     ROBFull <= ROBFullSignal;
-    process(clk)
+
+    process(clk,reset)
     begin
-       if (clk'event and clk = '0') then
+    	if(reset = '1') then
+    		q <= (others => (others => '0'));
+    		readPointer <= (others => '0');
+    		ReadPointerRotated <= '0';
+    		writePointer <= (others => '0');
+    		writePointerRotated <= '0';
+    		ROBFullSignal <= '0';
+    		ROBEmptySignal <= '1';
+
+       	elsif (clk'event and clk = '1') then
        		--read from decoding circuit
        		if (ROBFullSignal = '0') then
+       			q(to_integer(unsigned(writePointer))) <= instruction;
+       			writePointer <= writePointer + 1;
 
+       			if (writePointer = length-1) then
+       				writePointerRotated <= not writePointerRotated;
+       			end if;
 
+       			if (writePointer + 1 = readPointer) then
+       				ROBFullSignal <= '1';
+       			else
+       				ROBFullSignal <= '0';
+       			end if;
 
-       			q(tail) <= (others => '0')--some ciruit
        		end if;
 
-       elsif (clk'event and clk = '1') then
+       --elsif (clk'event and clk = '1') then
        		--some thing to be done
 
-       end if;
+      	end if;
     end process;
     
     

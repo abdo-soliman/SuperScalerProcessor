25f6fe4ecc3417cb9f1cf450440e92b03dbe3d70
yasmeenahmed <yeasmeenahmed@gmail.com>
2019-04-29 18:26:14 +0200

    added some constants

--- Constants.vhd
+++ Constants.vhd
@@ -7,6 +7,7 @@ constant PCCONTROLLER_NOP:    std_logic_vector(1 downto 0)			:= "00";
 constant PCCONTROLLER_NORMAL:    std_logic_vector(1 downto 0)		:= "01";
 constant PCCONTROLLER_ROB:    std_logic_vector(1 downto 0)			:= "10";
 constant STD_OPCODE:        std_logic_vector(4 downto 0)			:= "10011";
+constant LDD_OPCODE:        std_logic_Vector(4 downto 0)            := "10010";
 constant JMPZ_OPCODE:        std_logic_vector(4 downto 0)	    := "11100";
 constant JMPN_OPCODE:        std_logic_vector(4 downto 0)	    := "11101";
 constant JMPC_OPCODE:        std_logic_vector(4 downto 0)	        := "11110";
@@ -15,6 +16,9 @@ constant RET_OPCODE:        std_logic_vector(4 downto 0)	    := "11100";
 constant RTI_OPCODE:        std_logic_vector(4 downto 0)	    := "11010";
 constant PUSH_OPCODE:        std_logic_vector(4 downto 0)	    := "10000";
 constant POP_OPCODE:        std_logic_vector(4 downto 0)	    := "10001";
+constant ADD_OPCODE:        std_logic_vector(4 downto 0)	    := "00100";
+constant SUB_OPCODE:        std_logic_vector(4 downto 0)	    := "00101";
+
 
 
 

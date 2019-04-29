d40eef5075cf94b976858a5fb74c9cf05c26e7b0
yasmeenahmed <yeasmeenahmed@gmail.com>
2019-04-29 18:24:33 +0200

    added waiting tag functionality

--- Constants.vhd
+++ Constants.vhd
@@ -6,6 +6,18 @@ package Constants is
 constant PCCONTROLLER_NOP:    std_logic_vector(1 downto 0)			:= "00";
 constant PCCONTROLLER_NORMAL:    std_logic_vector(1 downto 0)		:= "01";
 constant PCCONTROLLER_ROB:    std_logic_vector(1 downto 0)			:= "10";
+constant STD_OPCODE:        std_logic_vector(4 downto 0)			:= "10011";
+constant JMPZ_OPCODE:        std_logic_vector(4 downto 0)	    := "11100";
+constant JMPN_OPCODE:        std_logic_vector(4 downto 0)	    := "11101";
+constant JMPC_OPCODE:        std_logic_vector(4 downto 0)	        := "11110";
+constant CALL_OPCODE:            std_logic_vector(4 downto 0)	    := "11000";
+constant RET_OPCODE:        std_logic_vector(4 downto 0)	    := "11100";
+constant RTI_OPCODE:        std_logic_vector(4 downto 0)	    := "11010";
+constant PUSH_OPCODE:        std_logic_vector(4 downto 0)	    := "10000";
+constant POP_OPCODE:        std_logic_vector(4 downto 0)	    := "10001";
+
+
+
 
 
 
--- ReorderBuffer.vhd
+++ ReorderBuffer.vhd
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 use IEEE.numeric_std.all;
+use work.constants.all;
 
 --length n and width m
 
@@ -39,11 +40,11 @@ architecture rtl of ReorderBuffer is
     signal ROBEmptySignal: 			std_logic := '1';
     signal opCodeSignal:            std_logic_vector(4 downto 0);
     -----------------------------Helper Functions-------------------------------
-    function OpCode(entry : std_logic_vector(width-1 downto 0) := (others => '0')) 	
+    function getOpCode(entry : std_logic_vector(width-1 downto 0) := (others => '0')) 	
     					return  std_logic_vector is
     begin
         return entry(47 downto 43);
-    end OpCode;
+    end getOpCode;
     ----------------------------------------------------------------------------
     function Value(entry : std_logic_vector(width-1 downto 0) := (others => '0')) 	
     					return  std_logic_vector is
@@ -99,7 +100,57 @@ architecture rtl of ReorderBuffer is
         --entry(0) <= '1' when OPcode(0) = '1'
         --    else '0';-- and( OPcode(2) )= '0' and( OPcode(3) )= '0' and ( OPcode(4) )= '0' );
     end setDone;
-    
+    ----------------------------------------------------------------------------
+    function checkStackFamily(opCode:   std_logic_vector(4 downto 0))
+                                return std_logic is
+    begin
+        if(opCode = PUSH_OPCODE or opCode = POP_OPCODE or opCode = RET_OPCODE or opCode = RTI_OPCODE or opCode = CALL_OPCODE) then
+            return '1';
+        else
+            return '0';
+        end if;
+    end checkStackFamily;
+    ----------------------------------------------------------------------------
+    function checkJmpFamily(opCode:   std_logic_vector(4 downto 0))
+                                return std_logic is
+    begin
+        if(opCode = JMPC_OPCODE or opCode = JMPN_OPCODE or opCode = JMPZ_OPCODE) then
+            return '1';
+        else
+            return '0';
+        end if;
+    end checkJmpFamily;
+    ----------------------------------------------------------------------------
+    function checkArithmeticFamily(opCode:   std_logic_vector(4 downto 0))
+                                return std_logic is
+    begin
+        if(opCode = ADD_OPCODE or opCode = SUB_OPCODE) then
+            return '1';
+        else
+            return '0';
+        end if;
+    end checkArithmeticFamily;
+    ----------------------------------------------------------------------------
+    function checkLoad(opCode:   std_logic_vector(4 downto 0))
+                                return std_logic is
+    begin
+        if(opCode = LDD_OPCODE) then
+            return '1';
+        else
+            return '0';
+        end if;
+    end checkLoad;
+    ----------------------------------------------------------------------------
+    function checkStore(opCode:   std_logic_vector(4 downto 0))
+                                return std_logic is
+    begin
+        if(opCode = STD_OPCODE) then
+            return '1';
+        else
+            return '0';
+        end if;
+    end checkStore;
+    ----------------------------------------------------------------------------
     procedure updateTagAluMemory(signal    entry:             inout       std_logic_vector(width-1 downto 0);
                                  signal    aluValue:          in          std_logic_vector(width-1 downto 0);
                                  signal    aluTag:            in          std_logic_vector(3 downto 0) ;
@@ -149,10 +200,101 @@ architecture rtl of ReorderBuffer is
             end if;
         --OPcode := OpCode(entry => entry);
         end updateTagAluMemory;
+    ----------------------------------------------------------------------------
+    procedure inputParser(signal    entry:          inout       std_logic_vector(width-1 downto 0);
+                          signal    q:			    inout	    qType;
+                          signal readPointer: 		in	        std_logic_vector(3 downto 0);
+                          signal writePointer: 		in	        std_logic_vector(3 downto 0);
+                          signal    ROBFullSignal:  in          std_logic )is
+                    
+    variable l: integer;
+    variable r: integer;
+    variable temp: integer;
+    variable stackFamily: std_logic;
+    variable jumpConditional: std_logic; 
+    variable entryOpCode: std_logic_vector(4 downto 0); 
+    variable loopOpCode: std_logic_vector(4 downto 0); 
+    variable typeOfOpCode: std_logic_vector(1 downto 0);--"00" if std,"01" if jmp, "10" if stack
+                
+    begin
+        entryOpCode := getOpCode(entry=> entry);
+        if (entryOpCode = STD_OPCODE or entryOpCode = JMPN_OPCODE or entryOpCode = JMPZ_OPCODE or entryOpCode = PUSH_OPCODE or entryOpCode = POP_OPcode or entryOpCode = RET_OPCODE or entryOpCode = RTI_OPCODE or entryOpCode = CALL_OPCODE)then
+            if(checkStore(opCode => entryOpCode) = '1') then
+                typeOfOpCode := "00";
+            elsif(checkStackFamily(opCode => entryOpCode) = '1') then
+                typeOfOpCode := "10";
+            elsif(checkJMPFamily(opCode => entryOpCode) = '1')then
+                typeOfOpCode := "01";
+            end if;
+            if(typeOfOpCode = "00" )then --case store find load
+                l := to_integer(unsigned(readPointer));
+                r := to_integer(unsigned(writePointer));
+                temp := to_integer(unsigned'('0' & ROBFullSignal));
+                
+                while( (l /= r) or temp = 1 ) loop
+                    if(temp = 1 and r = l+1) then
+                        temp := 0;
+                    end if;
+                    loopOpCode := getOpCode(entry => q(r) );
+                    if(checkLoad(opCode => loopOpCode) = '1') then
+                        entry(6 downto 3) <= std_logic_vector(to_unsigned(r , 4));
+                        exit;
+                    end if;
+                    if( r = 0 ) then
+                        r := 15;
+                    else
+                        r := r - 1; 
+                    end if;
+                end loop;
+            elsif(typeOfOpCode = "10")then -- jmp case
+                l := to_integer(unsigned(readPointer));
+                r := to_integer(unsigned(writePointer));
+                temp := to_integer(unsigned'('0' & ROBFullSignal));
+                
+                while( (l /= r) or temp = 1 ) loop
+                    if(temp = 1 and r = l+1) then
+                        temp := 0;
+                    end if;
+                    loopOpCode := getOpCode(entry => q(r) );
+                    if(checkJmpFamily(opCode => loopOpCode) = '1') then
+                        entry(6 downto 3) <= std_logic_vector(to_unsigned(r , 4));
+                        exit;
+                    end if;
+                    if( r = 0 ) then
+                        r := 15;
+                    else
+                        r := r - 1;
+                    end if;
+                end loop;
+            elsif(typeOfOpCode = "01") then --case stack
+                l := to_integer(unsigned(readPointer));
+                r := to_integer(unsigned(writePointer));
+                temp := to_integer(unsigned'('0' & ROBFullSignal));
+                
+                while( (l /= r) or temp = 1 ) loop
+                    if(temp = 1 and r = l+1) then
+                        temp := 0;
+                    end if;
+                    loopOpCode := getOpCode(entry => q(r) );
+                    if(checkArithmeticFamily(opCode => loopOpCode) = '1') then
+                        entry(6 downto 3) <= std_logic_vector(to_unsigned(r , 4));
+                        exit;
+                    end if;
+                    if( r = 0 ) then
+                        r := 15;
+                    else
+                        r := r - 1;
+                    end if;
+                end loop;
+            end if;
+        end if;                
+    end inputParser;
+
+
 
 begin
     output <= q(to_integer(unsigned(readPointer)));
-    opCodeSignal <= OpCode(q(to_integer(unsigned(readPointer))));
+    opCodeSignal <= getOpCode(q(to_integer(unsigned(readPointer))));
     --ROBFullSignal <= '1' when tail = head
     --				else '0';
     ROBFull <= ROBFullSignal;
